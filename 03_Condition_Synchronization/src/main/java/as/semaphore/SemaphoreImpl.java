30a2ea48d7c320ded23e1a161ab03d8302f62dcf
Melvin Johner <melvin.johner@students.fhnw.ch>
2021-04-06 11:48:07 +0200

    added 03

---
+++ 03_Condition_Synchronization/src/main/java/as/semaphore/SemaphoreImpl.java
@@ -0,0 +1,39 @@
+package as.semaphore;
+
+public final class SemaphoreImpl implements Semaphore {
+    private int value;
+    private final Object lock = new Object();
+
+    public SemaphoreImpl(int initial) {
+        if (initial < 0) throw new IllegalArgumentException();
+        value = initial;
+    }
+
+    @Override
+    public int available() {
+        synchronized (lock){
+            return value;
+        }
+    }
+
+    @Override
+    public void acquire() {
+        synchronized (lock){
+            while(value<=0){
+                try{ lock.wait();}
+                catch (InterruptedException e){}
+            }
+            value--;
+            lock.notifyAll();
+        }
+
+    }
+
+    @Override
+    public void release() {
+        synchronized (lock){
+            value++;
+            lock.notifyAll();
+        }
+    }
+}
